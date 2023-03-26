f6819d1224678ef502fd1ce9ae5765341ddf7749
Jasil Shan <jasilshan786@gmail.com>
2023-03-27 02:13:23 +0530

    romance and comedy genre added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./Components/NavBar/NavBar";
 import './App.css'
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
+import { action, comedy, originals, romance } from "./urls";
 
 function App() {
  
@@ -10,8 +11,10 @@ function App() {
   <div className="App">
     <NavBar/>
     <Banner/>
-    <RowPost title = 'Netflix Originals'/>
-    <RowPost title = ' Action' isSmall/>
+    <RowPost url={originals} title = 'Netflix Originals'/>
+    <RowPost url={action} title = 'Action' isSmall/>
+    <RowPost url={romance} title = 'Romance' isSmall/>
+    <RowPost url={comedy} title = 'Comedy' isSmall/>
   </div>
   );
 } 
--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,27 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "./RowPost.css";
 import axios from "../../axios";
-import { API_KEY, imageUrl } from "../../constants";
+import {imageUrl } from "../../constants";
 
 function RowPost(props) {
-  const [originals, setOriginals] = useState([]);
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-    axios
-      .get(`discover/tv?api_key=${API_KEY}&with_networks=213`)
-      .then((res) => {
+    axios.get(props.url).then((res) => {
         console.log(res.data);
-        setOriginals(res.data.results);
+        setMovies(res.data.results);
       })
       .catch((err) => {
         // alert("Network Error");
       });
-  }, []);
+  },[]);
 
   return (
     <div className="row">
       <h2>{props.title}</h2>
       <div className="posters">
-        {originals.map((obj) =>
+        {movies.map((obj) =>
           <img
             className={props.isSmall ? "smallPoster" : "poster"}
             src={`${imageUrl + obj.backdrop_path}`}
