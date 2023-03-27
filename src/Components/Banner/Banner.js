0ea25885e5e04562b890818f068a03f67a620c78
Jasil Shan <jasilshan786@gmail.com>
2023-03-27 10:54:37 +0530

    Random banner option added

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -4,11 +4,12 @@ import "./Banner.css";
 import axios from "../../axios";
 
 function Banner() {
+  const a = Math.floor(Math.random() * (20 - 1)) + 1;
   const [movie, setMovie] = useState()
   useEffect(() => {
     axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((res)=>{
       console.log(res.data.results[0])
-      setMovie(res.data.results[0])
+      setMovie(res.data.results[a])
     })
   }, [])
   
